bee44f82a683f77e5976dc8184f150ee517bcaed
Torsten Hache <Torsten.Hache@soprasteria.com>
2023-09-05 16:18:17 +0000

    Test

---
+++ app/LEARNING_JOURNEY_1.Risks/dist/test/integration/opaTests.qunit.js
@@ -0,0 +1,2 @@
+sap.ui.require(["sap/fe/test/JourneyRunner","LEARNINGJOURNEY1/Risks/test/integration/FirstJourney","LEARNINGJOURNEY1/Risks/test/integration/pages/RisksList","LEARNINGJOURNEY1/Risks/test/integration/pages/RisksObjectPage"],function(s,e,i,t){"use strict";var s=new s({launchUrl:sap.ui.require.toUrl("LEARNINGJOURNEY1/Risks")+"/index.html"});s.run({pages:{onTheRisksList:i,onTheRisksObjectPage:t}},e.run)});
+//# sourceMappingURL=opaTests.qunit.js.map
